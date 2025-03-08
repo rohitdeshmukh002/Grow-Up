053a6994b4851f7900552d591e11e6b869e78326
rohitdeshmukh002 <rohitdeshmukh649@gmail.com>
2025-03-08 17:01:22 +0530

    angular project is created and added all required dependancy

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyCbWgzc0NIZdVg8ndkoQXw7hg2WuYWFU-w",
+      authDomain: "grow-up-2cc97.firebaseapp.com",
+      projectId: "grow-up-2cc97",
+      storageBucket: "grow-up-2cc97.firebasestorage.app",
+      messagingSenderId: "801934346586",
+      appId: "1:801934346586:web:215f760c4afb65ce670b63"
+    }
+  };
